2807a0f55bf4976d5cba5aff3d585cbd94198e73
Raj Pawar <78217617+rajpawar45@users.noreply.github.com>
2023-01-20 03:49:09 -0600

    Update Ceaser_cypher.py

--- Ceaser_cypher.py
+++ Ceaser_cypher.py
@@ -1,3 +1,4 @@
+#function 
 def ceaser(ps,n):
     result = ""
     for i in range(len(ps)):
@@ -6,10 +7,13 @@ def ceaser(ps,n):
       else:
           result+=chr((ord(ps[i])+(n-97))%26+97)+chr(ord(ps[i])-60)
     return result
+# Code starts from here
 ps = input("Enter Any password: ")
 n = int(input("Enter pattern: "))
+# Output screen code start
 print("\nReport of Ceaser Cypher")
 print("*************************")
 print("Plain Password is : "+ ps)
 print("Pattern is : "+ str(n))
 print("Cipher Password is : " + ceaser(ps,n))
+# Created By Raj pawar
